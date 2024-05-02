eff510f763a8ec19c8ee53362bf3343fbc963efd
Franco <seranojuliofranciscojavier@gmail.com>
2024-05-02 18:47:42 -0500

    registrarPuntacion-controller actualizado para que cuando encuentre un usuario que ya está volviendo a registrar puntuación con una previa existente, se acutalice la puntuación

--- BACKEND/controllers/clasificacionController.js
+++ BACKEND/controllers/clasificacionController.js
@@ -1,34 +1,44 @@
 import Clasificacion from "../models/Clasificacion.js";
 import Usuario from "../models/Usuario.js";
-const registrarPuntuacion = async (req,res) => {
-    const {usuario,_id} = req.usuario;
-    const {puntaje} = req.body;
-    try{
-  const buscarUsuario = await Usuario.findOne({usuario});
-    if(!buscarUsuario){
-        const error = new Error("No se encontró el usuario");
-        return res.status(403).json({msg:error.message});
+const registrarPuntuacion = async (req, res) => {
+  const { usuario, _id } = req.usuario;
+  const { puntaje } = req.body;
+  try {
+    const buscarUsuario = await Usuario.findOne({ usuario });
+    if (!buscarUsuario) {
+      const error = new Error("No se encontró el usuario");
+      return res.status(403).json({ msg: error.message });
     }
-    const clasificacion = new Clasificacion({usuario:_id,puntaje});
+
+    const existeClasificacionPrevia = await Clasificacion.findOne({
+      usuario: _id,
+    });
+    //Se actualiza la clasficacion del usuario
+    if (existeClasificacionPrevia) {
+      existeClasificacionPrevia.puntaje =
+        puntaje || existeClasificacionPrevia.puntaje;
+      await existeClasificacionPrevia.save();
+      return res.json({ msg: "Se ha actualizado la puntuación del usuario" });
+    }
+    const clasificacion = new Clasificacion({ usuario: _id, puntaje });
     await clasificacion.save();
-    return res.json({msg:"Se registró la puntuación correctamente"});
-    }catch(error) {
-        console.log(error)
+    return res.json({ msg: "Se registró la puntuación correctamente" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const obtenerPuntuaciones = async (req, res) => {
+  const { usuario } = req.usuario;
+  try {
+    const buscarUsuario = await Usuario.findOne({ usuario });
+    if (!buscarUsuario) {
+      const error = new Error("No se encontró el usuario");
+      return res.status(403).json({ msg: error.message });
     }
-  
-}
-const obtenerPuntuaciones = async(req,res) => {
-    const {usuario} = req.usuario;
-    try{
-        const buscarUsuario = await Usuario.findOne({usuario});
-        if(!buscarUsuario){
-            const error = new Error("No se encontró el usuario");
-            return res.status(403).json({msg:error.message});
-        }
-        const puntuaciones = await Clasificacion.find();
-        return res.json({puntuaciones})
-    }catch(error){
-        console.log(error)
-    }
-}
-export {registrarPuntuacion,obtenerPuntuaciones}
+    const puntuaciones = await Clasificacion.find();
+    return res.json({ puntuaciones });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export { registrarPuntuacion, obtenerPuntuaciones };
